c1637998aafab36413840fca01c37aecb1c1d43d
Kareem Khater <kareem.khater360@gmail.com>
2023-08-05 19:44:52 +0300

    OmniFood Order

---
+++ BackEnd/src/main/java/com/omnifood/omnifoodorder/OmnifoodOrderApplication.java
@@ -0,0 +1,13 @@
+package com.omnifood.omnifoodorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnifoodOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnifoodOrderApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/omnifood/omnifoodorder/OmnifoodOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnifood.omnifoodorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnifoodOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
